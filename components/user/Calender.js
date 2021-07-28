1329aed46d63981b2294c8ca2e091522e85024c2
Mubeen Hussain <mubeenhussain8@gmail.com>
2021-07-28 22:24:24 +0500

    fix error in tooltip

--- components/user/Calender.js
+++ components/user/Calender.js
@@ -4,7 +4,8 @@ import {
   fade,
   lighten,
 } from "@material-ui/core/styles/colorManipulator";
-import {BookIcon,SubjectIcon} from '@material-ui/icons/Book';
+import BookIcon from '@material-ui/icons/Book';
+import SubjectIcon from '@material-ui/icons/Subject';
 import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
@@ -13,7 +14,10 @@ import {
   Toolbar,
   DateNavigator,
   AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import { Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { useRouter } from "next/router";
 import axios from 'axios';
@@ -21,7 +25,7 @@ import { baseURL } from "../../env";
 import Alert from '@material-ui/lab/Alert';
 import { useDispatch,useSelector } from "react-redux";
 import * as actions from './redux/actions'
-import {ListItem,ListItemText,ListItemAvatar,Avatar,Box,Paper,Grid,Typography,Button,CircularProgress} from '@material-ui/core';
+import {ListItem,ListItemText,ListItemAvatar,Avatar,Box,Paper,Grid,Typography,CircularProgress} from '@material-ui/core';
 
 
 const getBorder = (theme) =>
@@ -31,115 +35,137 @@ const getBorder = (theme) =>
       : darken(fade(theme.palette.divider, 1), 0.68)
   }`;
 
-const styles = (theme) => ({
-  cell: {
-    // color: "#78909C!important",
-    position: "relative",
-    userSelect: "none",
-    verticalAlign: "top",
-    padding: 0,
-    height: 100,
-    borderLeft: getBorder(theme),
-    "&:first-child": {
-      borderLeft: "none",
+
+  const styles = (theme) => ({
+    cell: {
+      color: "#78909C!important",
+      position: "relative",
+      userSelect: "none",
+      verticalAlign: "top",
+      padding: 0,
+      height: 100,
+      borderLeft: getBorder(theme),
+      "&:first-child": {
+        borderLeft: "none",
+      },
+      "&:last-child": {
+        paddingRight: 0,
+      },
+      "tr:last-child &": {
+        borderBottom: "none",
+      },
+      "&:hover": {
+        backgroundColor: "white",
+      },
+      "&:focus": {
+        backgroundColor: fade(theme.palette.primary.main, 0.15),
+        outline: 0,
+      },
     },
-    "&:last-child": {
-      paddingRight: 0,
+    content: {
+      display: "flex",
+      justifyContent: "center",
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+      alignItems: "center",
     },
-    "tr:last-child &": {
-      borderBottom: "none",
+    text: {
+      padding: "0.5em",
+      textAlign: "center",
     },
-    "&:hover": {
-      backgroundColor: "white",
-    }
-  },
-  content: {
-    display: "flex",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%",
-    position: "absolute",
-    alignItems: "center",
-  },
-  text: {
-    padding: "0.5em",
-    textAlign: "center",
-  },
-  opacity: {
-    opacity: "0.5",
-  },
-  appointment: {
-    borderRadius: "10px",
-    "&:hover": {
-      opacity: 0.6,
-      backgroundColor:'#65d4e0'
+    sun: {
+      color: "#FFEE58",
     },
-    backgroundColor: "#50bbc7",
-  },
-  bookedAppointment:{
-    borderRadius: "10px",
-    color: "white",
-    backgroundColor: "#7b40c0",
-    "&:hover": {
-      opacity: 0.6,
-      backgroundColor:'#6146AC'
+    cloud: {
+      color: "#90A4AE",
     },
-  },
-  apptContent: {
-    "&>div>div": {
-      whiteSpace: "normal !important",
-      lineHeight: 1.2,
+    rain: {
+      color: "#4FC3F7",
     },
-   
-  },
-  flexibleSpace: {
-    flex: "none",
-  },
-  flexContainer: {
-    display: "flex",
-    alignItems: "center",
-  },
-  tooltipContent: {
-    padding: theme.spacing(3, 1),
-    paddingTop: 0,
-    // backgroundColor: theme.palette.background.paper,
-    boxSizing: "border-box",
-    width: "400px",
-  },
-  tooltipText: {
-    ...theme.typography.body2,
-    display: "inline-block",
-  },
-  title: {
-    ...theme.typography.h6,
-    color: theme.palette.text.secondary,
-    fontWeight: theme.typography.fontWeightBold,
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
-  },
-  icon: {
-    color: theme.palette.action.active,
-    verticalAlign: "middle",
-  },
-  circle: {
-    width: theme.spacing(4.5),
-    height: theme.spacing(4.5),
-    verticalAlign: "super",
-  },
-  textCenter: {
-    textAlign: "center",
-  },
-  dateAndTitle: {
-    lineHeight: 1.1,
-  },
-  titleContainer: {
-    paddingBottom: theme.spacing(2),
-  },
-  container: {
-    paddingBottom: theme.spacing(1.5),
-  },
-});
+    sunBack: {
+      backgroundColor: "#FFFDE7",
+    },
+    cloudBack: {
+      backgroundColor: "#ECEFF1",
+    },
+    rainBack: {
+      backgroundColor: "#E1F5FE",
+    },
+    opacity: {
+      opacity: "0.5",
+    },
+    appointment: {
+      borderRadius: "10px",
+      "&:hover": {
+        opacity: 0.6,
+        backgroundColor:'#65d4e0'
+      },
+      backgroundColor: "#50bbc7",
+    },
+    bookedAppointment:{
+      borderRadius: "10px",
+      color: "white",
+      backgroundColor: "#7b40c0",
+      "&:hover": {
+        opacity: 0.6,
+        backgroundColor:'#6146AC'
+      },
+    },
+    apptContent: {
+      "&>div>div": {
+        whiteSpace: "normal !important",
+        lineHeight: 1.2,
+      },
+    },
+    flexibleSpace: {
+      flex: "none",
+    },
+    flexContainer: {
+      display: "flex",
+      alignItems: "center",
+    },
+    tooltipContent: {
+      padding: theme.spacing(3, 1),
+      paddingTop: 0,
+      backgroundColor: theme.palette.background.paper,
+      boxSizing: "border-box",
+      width: "400px",
+    },
+    tooltipText: {
+      ...theme.typography.body2,
+      display: "inline-block",
+    },
+    title: {
+      ...theme.typography.h6,
+      color: theme.palette.text.secondary,
+      fontWeight: theme.typography.fontWeightBold,
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+    },
+    icon: {
+      color: theme.palette.action.active,
+      verticalAlign: "middle",
+    },
+    circle: {
+      width: theme.spacing(4.5),
+      height: theme.spacing(4.5),
+      verticalAlign: "super",
+    },
+    textCenter: {
+      textAlign: "center",
+    },
+    dateAndTitle: {
+      lineHeight: 1.1,
+    },
+    titleContainer: {
+      paddingBottom: theme.spacing(2),
+    },
+    container: {
+      paddingBottom: theme.spacing(1.5),
+    },
+  });
 
 const style = ({ palette }) => ({
   icon: {
@@ -168,24 +194,26 @@ const style = ({ palette }) => ({
 
 const Appointment = withStyles(styles, { name: "Appointment" })(
   ({ classes, ...restProps }) => (
-    <>
+    
+    <div>
   {   
-      restProps.data.availability=== false?
+   
+    restProps.data.availability=== false?
       <Appointments.Appointment
                 {...restProps}
                 className={classes.bookedAppointment}      
-                onClick={() => null}     
+                onClick={() => console.log("",restProps)}     
             />
       :
-      <>
+      
       <Appointments.Appointment
                 {...restProps}
                 className={classes.appointment}      
                 // onClick={() => console.log("Rest Props 2: ", restProps)}     
             />
-      </>  
+        
   }   
-    </>
+    </div>
     
   )
 );
@@ -195,7 +223,6 @@ const AppointmentContent = withStyles(styles, { name: "AppointmentContent" })(
     <Appointments.AppointmentContent
       {...restProps}
       className={classes.apptContent}
-      // onClick={() => console.log("Rest Props 2: ", restProps)}
     />
   )
 );
@@ -212,8 +239,7 @@ const FlexibleSpace = withStyles(styles, { name: "ToolbarRoot" })(
               <Box  borderRadius="50%"  style={{ width:'20px',height:'20px',backgroundColor:'#50bbc7',marginRight:'10px',marginLeft:'10px' }}>
               </Box>
               <ListItemText primary="Available"  />
-        </ListItem>
-        
+        </ListItem>      
        
         <Typography variant="h5" >
           <img src="/logo.png" style={{ width: "100px", height: "100px" }} />
@@ -296,6 +322,7 @@ export default class Calender extends React.PureComponent {
 
   render() {
     const { data, open } = this.state;
+    console.log(data.schedule,"Data schedule")
     console.log(data.schedule?.length,"schedule length")
     return (
      
@@ -307,11 +334,11 @@ export default class Calender extends React.PureComponent {
             :
           data.schedule?.length>0?
           <>
-            <Typography variant="h6" color="primary">Total Slots: {data.schedule.length}</Typography>
+            {/* <Typography variant="h6" color="primary">Total Slots: {data.schedule.length}</Typography> */}
               <Paper style={{ border: "1px solid #E0E0E0" }} elevation={3}>             
               <Scheduler data={data.schedule}>
                 <ViewState  />           
-                <WeekView startDayHour={9} endDayHour={17} />
+                <WeekView startDayHour={9} endDayHour={19} />
                 <Appointments
                   appointmentComponent={Appointment}
                   appointmentContentComponent={AppointmentContent}
